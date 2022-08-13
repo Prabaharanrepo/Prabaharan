04263265b133795301cb821269a05af3a3ecf2e1
Prabaharan <prabaharan.auroville@gmail.com>
2022-08-13 13:04:02 +0530

    items

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime
+from typing import Union
+from fastapi.responses import ORJSONResponse
+
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -19,6 +27,14 @@ app.add_middleware(
 )
 
 
+
 @app.get("/")
 async def main():
     return {"message": "Hello World"}
+
+
+
+
+@app.get("/items/", response_class=ORJSONResponse)
+async def read_items():
+    return [{"item_id": "Foo"}]
